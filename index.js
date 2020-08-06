be69ba61f5db53608a93b13f4b9a877a53a0a390
jaeheng <jaeheng@qq.com>
2020-08-06 15:37:26 +0800

    0.1.4

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import VueSignature from '@/components/vue-signature'
+import VueSignature from './src/components/vue-signature'
 
 if (typeof window !== 'undefined' && window.Vue) {
   window.Vue.component('vue-signature', VueSignature)
